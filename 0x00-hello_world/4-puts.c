3e9e9a2d1bc02e7c254ae9deb63ef87813269a41
Emmynugwa <remodafrikaofficial@gmail.com>
2023-06-25 10:09:55 -0700

    4-puts.c edit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - Programming is like building a multilingual puzzle
- * Return: 0
- */
+* main - Programming is like building a multilingual puzzle
+* Return: 0
+*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-Return: (0);
+return (0);
 }
